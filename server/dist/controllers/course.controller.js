e3cf10f1f5d5ba76f0a7a5cd359d9451426a1ca4
saumil athya <saumilathya@gmail.com>
2025-06-27 18:38:47 +0530

    get-courses fix it

---
+++ server/dist/controllers/course.controller.js
@@ -0,0 +1,222 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deleteCourse = exports.getAdminAllCourses = exports.addQuestion = exports.getCourseByUser = exports.getAllCourses = exports.getSingleCourse = exports.uploadCourse = void 0;
+const catchAsyncErrors_1 = require("../middleware/catchAsyncErrors");
+const ErrorHandler_1 = __importDefault(require("../utils/ErrorHandler"));
+const course_model_1 = __importDefault(require("../models/course.model"));
+const mongoose_1 = __importDefault(require("mongoose"));
+// upload course
+exports.uploadCourse = (0, catchAsyncErrors_1.CatchAsyncError)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const data = req.body;
+        const course = yield course_model_1.default.create(data);
+        res.status(201).json({
+            success: true,
+            course,
+        });
+    }
+    catch (error) {
+        return next(new ErrorHandler_1.default(error.message, 500));
+    }
+}));
+// edit course
+// export const editCourse = CatchAsyncError(
+//   async (req: Request, res: Response, next: NextFunction) => {
+//     try {
+//       const data = req.body;
+//       const thumbnail = data.thumbnail;
+//       const courseId = req.params.id;
+//       const courseData = (await CourseModel.findById(courseId)) as any;
+//       if (thumbnail && !thumbnail.startsWith("https")) {
+//         await cloudinary.v2.uploader.destroy(courseData.thumbnail.public_id);
+//         const myCloud = await cloudinary.v2.uploader.upload(thumbnail, {
+//           folder: "courses",
+//         });
+//         data.thumbnail = {
+//           public_id: myCloud.public_id,
+//           url: myCloud.secure_url,
+//         };
+//       }
+//       if (thumbnail.startsWith("https")) {
+//         data.thumbnail = {
+//           public_id: courseData?.thumbnail.public_id,
+//           url: courseData?.thumbnail.url,
+//         };
+//       }
+//       const course = await CourseModel.findByIdAndUpdate(
+//         courseId,
+//         {
+//           $set: data,
+//         },
+//         { new: true }
+//       );
+//       res.status(201).json({
+//         success: true,
+//         course,
+//       });
+//     } catch (error: any) {
+//       return next(new ErrorHandler(error.message, 500));
+//     }
+//   }
+// );
+// get single course --- without purchasing
+exports.getSingleCourse = (0, catchAsyncErrors_1.CatchAsyncError)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const courseId = req.params.id;
+        const course = yield course_model_1.default.findById(courseId).select("-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links");
+        res.status(200).json({
+            success: true,
+            course,
+        });
+    }
+    catch (error) {
+        return next(new ErrorHandler_1.default(error.message, 500));
+    }
+}));
+// get all courses --- without purchasing
+exports.getAllCourses = (0, catchAsyncErrors_1.CatchAsyncError)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const courses = yield course_model_1.default.find().select("-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links");
+        res.status(200).json({
+            success: true,
+            courses,
+        });
+    }
+    catch (error) {
+        return next(new ErrorHandler_1.default(error.message, 500));
+    }
+}));
+// get course content -- only for valid user
+exports.getCourseByUser = (0, catchAsyncErrors_1.CatchAsyncError)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    var _a;
+    try {
+        const userCourseList = (_a = req.user) === null || _a === void 0 ? void 0 : _a.courses;
+        const courseId = req.params.id;
+        const courseExists = userCourseList === null || userCourseList === void 0 ? void 0 : userCourseList.find((course) => course._id.toString() === courseId);
+        if (!courseExists) {
+            return next(new ErrorHandler_1.default("You are not eligible to access this course", 404));
+        }
+        const course = yield course_model_1.default.findById(courseId);
+        const content = course === null || course === void 0 ? void 0 : course.courseData;
+        res.status(200).json({
+            success: true,
+            content,
+        });
+    }
+    catch (error) {
+        return next(new ErrorHandler_1.default(error.message, 500));
+    }
+}));
+exports.addQuestion = (0, catchAsyncErrors_1.CatchAsyncError)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    var _a;
+    try {
+        const { question, courseId, contentId } = req.body;
+        const course = yield course_model_1.default.findById(courseId);
+        if (!mongoose_1.default.Types.ObjectId.isValid(contentId)) {
+            return next(new ErrorHandler_1.default("Invalid content id", 400));
+        }
+        const couseContent = (_a = course === null || course === void 0 ? void 0 : course.courseData) === null || _a === void 0 ? void 0 : _a.find((item) => item._id.equals(contentId));
+        if (!couseContent) {
+            return next(new ErrorHandler_1.default("Invalid content id", 400));
+        }
+        // create a new question object
+        const newQuestion = {
+            user: req.user,
+            question,
+            questionReplies: [],
+        };
+        // add this question to our course content
+        couseContent.questions.push(newQuestion);
+        //   await NotificationModel.create({
+        //     user: req.user?._id,
+        //     title: "New Question Received",
+        //     message: `You have a new question in ${couseContent.title}`,
+        //   });
+        // save the updated course
+        yield (course === null || course === void 0 ? void 0 : course.save());
+        res.status(200).json({
+            success: true,
+            course,
+        });
+    }
+    catch (error) {
+        return next(new ErrorHandler_1.default(error.message, 500));
+    }
+}));
+// export const addReplyToReview = CatchAsyncError(
+//   async (req: Request, res: Response, next: NextFunction) => {
+//     try {
+//       const { comment, courseId, reviewId } = req.body as IAddReviewData;
+//       const course = await CourseModel.findById(courseId);
+//       if (!course) {
+//         return next(new ErrorHandler("Course not found", 404));
+//       }
+//       const review = course?.reviews?.find(
+//         (rev: any) => rev._id.toString() === reviewId
+//       );
+//       if (!review) {
+//         return next(new ErrorHandler("Review not found", 404));
+//       }
+//       const replyData: any = {
+//         user: req.user,
+//         comment,
+//         createdAt: new Date().toISOString(),
+//         updatedAt: new Date().toISOString(),
+//       };
+//       if (!review.commentReplies) {
+//         review.commentReplies = [];
+//       }
+//       review.commentReplies?.push(replyData);
+//       await course?.save();
+//       res.status(200).json({
+//         success: true,
+//         course,
+//       });
+//     } catch (error: any) {
+//       return next(new ErrorHandler(error.message, 500));
+//     }
+//   }
+// );
+// get all courses --- only for admin
+exports.getAdminAllCourses = (0, catchAsyncErrors_1.CatchAsyncError)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const courses = yield course_model_1.default.find().sort({ createdAt: -1 });
+        res.status(201).json({
+            success: true,
+            courses,
+        });
+    }
+    catch (error) {
+        return next(new ErrorHandler_1.default(error.message, 400));
+    }
+}));
+// Delete Course --- only for admin
+exports.deleteCourse = (0, catchAsyncErrors_1.CatchAsyncError)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { id } = req.params;
+        const course = yield course_model_1.default.findById(id);
+        if (!course) {
+            return next(new ErrorHandler_1.default("course not found", 404));
+        }
+        yield course.deleteOne({ id });
+        res.status(200).json({
+            success: true,
+            message: "course deleted successfully",
+        });
+    }
+    catch (error) {
+        return next(new ErrorHandler_1.default(error.message, 400));
+    }
+}));
---
+++ server/dist/server.js
@@ -0,0 +1,38 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+const express_1 = __importDefault(require("express"));
+const http_1 = __importDefault(require("http"));
+const db_1 = require("./db/db");
+const error_1 = require("./middleware/error");
+const user_route_1 = __importDefault(require("./routes/user.route"));
+const cors_1 = __importDefault(require("cors"));
+const course_route_1 = __importDefault(require("./routes/course.route"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+const server = http_1.default.createServer(app);
+const PORT = process.env.PORT || 3000;
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+// cookie parser
+// app.use(cookieParser());
+app.use((0, cors_1.default)({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+}));
+app.use("/api/v1", user_route_1.default);
+app.use("/api/v1", course_route_1.default);
+app.get("/text", (req, res) => {
+    res.status(404).json({
+        success: true,
+        message: "Api is not working",
+    });
+});
+server.listen(PORT, () => {
+    (0, db_1.connectDb)();
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
+app.use(error_1.ErrorMiddleware);
